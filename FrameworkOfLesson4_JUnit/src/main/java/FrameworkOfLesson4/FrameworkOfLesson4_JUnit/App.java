f954c1b6418f39fa3097fab61806e1a673c121de
Avedon-007 <A_N_D_Y-007@mail.ru>
2017-02-26 23:06:32 +0200

    Create Repository for Framework of Lesson 4

---
+++ FrameworkOfLesson4_JUnit/src/main/java/FrameworkOfLesson4/FrameworkOfLesson4_JUnit/App.java
@@ -0,0 +1,13 @@
+package FrameworkOfLesson4.FrameworkOfLesson4_JUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
